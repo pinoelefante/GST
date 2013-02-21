aeaf4b0c7d676dde4671c8879853985f6596d323
pino <pinoelefante@hotmail.it>
2013-02-21 03:15:47 +0100

    nuovi file

---
+++ src/Naming/CaratteristicheFile.java
@@ -0,0 +1,38 @@
+package Naming;
+
+public class CaratteristicheFile {
+	private int stagione, episodio;
+	private boolean hd720, repack, proper;
+	public CaratteristicheFile() {}
+	public int getStagione() {
+		return stagione;
+	}
+	public void setStagione(int stagione) {
+		this.stagione = stagione;
+	}
+	public int getEpisodio() {
+		return episodio;
+	}
+	public void setEpisodio(int episodio) {
+		this.episodio = episodio;
+	}
+	public boolean is720p() {
+		return hd720;
+	}
+	public void set720p(boolean hd720) {
+		this.hd720 = hd720;
+	}
+	public boolean isRepack() {
+		return repack;
+	}
+	public void setRepack(boolean repack) {
+		this.repack = repack;
+	}
+	public boolean isProper() {
+		return proper;
+	}
+	public void setProper(boolean proper) {
+		this.proper = proper;
+	}
+	
+}
---
+++ src/Naming/Naming.java
@@ -0,0 +1,17 @@
+package Naming;
+
+public class Naming {
+	/*
+	 * S00E00
+	 * 0x00
+	 * 00x00
+	 * 0.0
+	 * Part 0 - singola serie
+	 * 0of0 -singola serie
+	 */
+	
+	public static CaratteristicheFile parseMagnetName(String nome){
+		return null;
+	}
+
+}
