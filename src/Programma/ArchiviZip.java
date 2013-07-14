f51b46384c41567ae6efde68cfbc6542616ae946
Pino <Pino@Pino-PC>
2013-07-14 15:40:07 +0200

    - Estrazione archivi

--- src/Programma/ArchiviZip.java
+++ src/Programma/ArchiviZip.java
@@ -2,81 +2,116 @@ package Programma;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.ArrayList;
 
-import Programma.ManagerException;
-
-import net.sf.sevenzipjbinding.ExtractOperationResult;
 import net.sf.sevenzipjbinding.ISevenZipInArchive;
+import net.sf.sevenzipjbinding.PropID;
 import net.sf.sevenzipjbinding.SevenZip;
 import net.sf.sevenzipjbinding.SevenZipException;
 import net.sf.sevenzipjbinding.SevenZipNativeInitializationException;
 import net.sf.sevenzipjbinding.impl.RandomAccessFileInStream;
-import net.sf.sevenzipjbinding.simple.ISimpleInArchive;
-import net.sf.sevenzipjbinding.simple.ISimpleInArchiveItem;
-
 
 public class ArchiviZip {
-	public static void main(String[] args) {
-		try {
-            SevenZip.initSevenZipFromPlatformJAR();
-            System.out.println("7-Zip-JBinding library was initialized");
-            estrai("D:\\SerieTV\\Baby Daddy\\Baby.Daddy.S02E07.HDTV.x264-ASAP.zip", ".srt", "ciao_pino", "D:\\SerieTV\\Baby Daddy\\");
-        } 
-		catch (SevenZipNativeInitializationException e) {
-            e.printStackTrace();
+	private static boolean init;
+
+	private static void instance() {
+		if (!init) {
+			try {
+				SevenZip.initSevenZipFromPlatformJAR();
+				init = true;
+			}
+			catch (SevenZipNativeInitializationException e) {
+				ManagerException.registraEccezione(e);
+			}
 		}
 	}
-	public static void estrai(String archivio, final String estensione, final String rename, final String cartella_destinazione){
-		RandomAccessFile randomAccessFile = null;
-        ISevenZipInArchive inArchive = null;
-        
-        try{
-        	randomAccessFile=new RandomAccessFile(archivio, "r");
-        	inArchive=SevenZip.openInArchive(null, new RandomAccessFileInStream(randomAccessFile));
-        	
-            ISimpleInArchive simpleInArchive = inArchive.getSimpleInterface();
-            
-            ArrayList<ISimpleInArchiveItem> estrarre = new ArrayList<ISimpleInArchiveItem>();
-            for(ISimpleInArchiveItem item : simpleInArchive.getArchiveItems()){
-            	if(!item.isFolder()){
-            		if(item.getPath().toLowerCase().endsWith(estensione.toLowerCase())){
-            			estrarre.add(item);
-            		}
-            	}
-            }
-            
-            for(int i=0;i<estrarre.size();i++){
-            	ISimpleInArchiveItem item=estrarre.get(i);
-            	ExtractOperationResult res = item.extractSlow(new SevenZipExtractor(estensione, rename, cartella_destinazione, i));
-            	System.out.println(res.name());
-            }
-            
-        }
-        catch(IOException e){
-        	e.printStackTrace();
-        } 
-        catch (SevenZipException e) {
-        	ManagerException.registraEccezione(e);
+
+	public static void main(String[] args) {
+		try {
+			SevenZip.initSevenZipFromPlatformJAR();
+			System.out.println("7-Zip-JBinding library was initialized");
+			estrai("D:\\SerieTV\\Baby Daddy\\Baby.Daddy.S02E07.HDTV.x264-ASAP.zip", ".srt", "ciao_pino", "D:\\SerieTV\\Baby Daddy\\");
+			// estrai_tutto("C:\\Users\\Pino\\Desktop\\vlc_win64.7z","D:\\SerieTV\\VLC");
+		}
+		catch (SevenZipNativeInitializationException e) {
 			e.printStackTrace();
 		}
-        finally {
-        	if (inArchive != null) {
-                try {
-                    inArchive.close();
-                } 
-                catch (SevenZipException e) {
-                    System.err.println("Error closing archive: " + e);
-                }
-            }
-            if (randomAccessFile != null) {
-                try {
-                    randomAccessFile.close();
-                } 
-                catch (IOException e) {
-                    System.err.println("Error closing file: " + e);
-                }
-            }
-        }
+	}
+
+	public static void estrai_tutto(String archivio, String dir_dest) {
+		instance();
+		RandomAccessFile randomAccessFile = null;
+		ISevenZipInArchive inArchive = null;
+		try {
+			randomAccessFile = new RandomAccessFile(archivio, "r");
+			inArchive = SevenZip.openInArchive(null, new RandomAccessFileInStream(randomAccessFile));
+
+			int[] in = new int[inArchive.getNumberOfItems()];
+			for (int i = 0; i < in.length; i++) {
+				in[i] = i;
+			}
+			inArchive.extract(in, false, new MySevenZipCallBack(inArchive, dir_dest, ""));
+		}
+		catch (Exception e) {
+			System.err.println("Error occurs: " + e);
+			System.exit(1);
+		}
+		finally {
+			if (inArchive != null) {
+				try {
+					inArchive.close();
+				}
+				catch (SevenZipException e) {
+					System.err.println("Error closing archive: " + e);
+				}
+			}
+			if (randomAccessFile != null) {
+				try {
+					randomAccessFile.close();
+				}
+				catch (IOException e) {
+					System.err.println("Error closing file: " + e);
+				}
+			}
+		}
+	}
+
+	public static void estrai(String archivio, final String estensione, final String rename, final String cartella_destinazione) {
+		RandomAccessFile randomAccessFile = null;
+		ISevenZipInArchive inArchive = null;
+		try {
+			randomAccessFile = new RandomAccessFile(archivio, "r");
+			inArchive = SevenZip.openInArchive(null, new RandomAccessFileInStream(randomAccessFile));
+
+			int[] in = new int[inArchive.getNumberOfItems()];
+			int i;
+			for (i = 0; i < in.length; i++) {
+				String path = (String) inArchive.getProperty(i, PropID.PATH);
+				if (path.toLowerCase().endsWith(estensione.toLowerCase()))
+					in[i] = i;
+			}
+			System.arraycopy(in, 0, in, 0, i);
+			inArchive.extract(in, false, new MySevenZipCallBack(inArchive, cartella_destinazione, rename));
+		}
+		catch (Exception e) {
+			System.err.println("Error occurs: " + e);
+		}
+		finally {
+			if (inArchive != null) {
+				try {
+					inArchive.close();
+				}
+				catch (SevenZipException e) {
+					System.err.println("Error closing archive: " + e);
+				}
+			}
+			if (randomAccessFile != null) {
+				try {
+					randomAccessFile.close();
+				}
+				catch (IOException e) {
+					System.err.println("Error closing file: " + e);
+				}
+			}
+		}
 	}
 }
---
+++ src/Programma/MySevenZipCallBack.java
@@ -0,0 +1,134 @@
+package Programma;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import net.sf.sevenzipjbinding.ExtractAskMode;
+import net.sf.sevenzipjbinding.ExtractOperationResult;
+import net.sf.sevenzipjbinding.IArchiveExtractCallback;
+import net.sf.sevenzipjbinding.ISequentialOutStream;
+import net.sf.sevenzipjbinding.ISevenZipInArchive;
+import net.sf.sevenzipjbinding.PropID;
+import net.sf.sevenzipjbinding.SevenZipException;
+
+public class MySevenZipCallBack implements IArchiveExtractCallback {
+        private int index;
+        private boolean skipExtraction;
+        private ISevenZipInArchive inArchive;
+        private String destinazione;
+        private String rename;
+        
+        private FileWriter fw;
+        
+        public MySevenZipCallBack(ISevenZipInArchive inArchive, String destinazione, String rename) {
+            this.inArchive = inArchive;
+            this.destinazione=destinazione;
+            this.rename=rename;
+        }
+
+        public ISequentialOutStream getStream(int index, ExtractAskMode extractAskMode) throws SevenZipException {
+            this.index = index;
+            skipExtraction = (Boolean) inArchive.getProperty(index, PropID.IS_FOLDER);
+            if (skipExtraction || extractAskMode != ExtractAskMode.EXTRACT) {
+                return null;
+            }
+            return new ISequentialOutStream() {
+            	
+            	private String nome_file="";
+            	
+                public int write(byte[] data) throws SevenZipException {
+                	if(nome_file.isEmpty()){
+                		nome_file=(String) inArchive.getProperty(getIndex(), PropID.PATH);
+                		String cartella="";
+                		if(nome_file.contains(File.separator)){
+                			cartella=nome_file.substring(0, nome_file.lastIndexOf(File.separator));
+                		}
+                		
+                		cartella=destinazione+(destinazione.endsWith(File.separator)?"":File.separator)+cartella;
+                		File dir_dest=new File(cartella);
+                		if(!dir_dest.exists())
+                			dir_dest.mkdirs();
+                		
+                		if(!rename.isEmpty()){
+                			if(nome_file.contains(".")){
+                				String estensione=nome_file.substring(nome_file.lastIndexOf("."));
+                				if(nome_file.contains(File.separator)){
+                					nome_file=nome_file.substring(0, nome_file.lastIndexOf(File.separator)+1)+rename+estensione;
+                				}
+                				else {
+                					nome_file=rename+estensione;
+                				}
+                			}
+                			System.out.println(nome_file);
+                		}
+                		String nome_file_dest=destinazione+(destinazione.endsWith(File.separator)?"":File.separator)+nome_file;
+                		File f=new File(nome_file_dest);
+                		if(f.exists()){
+                			f.delete();
+                			try {
+								f.createNewFile();
+							} 
+                			catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+                		}
+                		
+                		try {
+							fw=new FileWriter(destinazione+(destinazione.endsWith(File.separator)?"":File.separator)+nome_file);
+						} 
+                		catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                	}
+                	if(fw!=null){
+                		try {
+							fw.append(new String(data));
+						} 
+                		catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                	}
+                	
+                    return data.length; // Return amount of proceed data
+                }
+            };
+            
+        }
+        public int getIndex(){
+        	return index;
+        }
+        public void toFile(){
+        	
+        }
+        public void prepareOperation(ExtractAskMode extractAskMode) throws SevenZipException {  }
+
+        public void setOperationResult(ExtractOperationResult extractOperationResult) throws SevenZipException {
+            if (skipExtraction) {
+                return;
+            }
+            if (extractOperationResult != ExtractOperationResult.OK) {
+                System.err.println("Extraction error");
+            } 
+            else {
+                System.out.println(String.format("%s", inArchive.getProperty(index, PropID.PATH)));
+                if(fw!=null){
+                	try {
+						fw.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                	fw=null;
+                }
+            }
+        }
+
+        public void setCompleted(long completeValue) throws SevenZipException { }
+
+        public void setTotal(long total) throws SevenZipException { }
+ 
+}
